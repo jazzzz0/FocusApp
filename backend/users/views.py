9a9079d365e7b2233307a7b88a1d49692833c650
jazzzz0 <maezjazmin@gmail.com>
2025-07-24 15:53:46 -0300

    feat(users): implementar endpoint de logout con blacklist de JWT
    - Se agregó una vista personalizada para logout que invalida el refresh token usando la blacklist de SimpleJWT.
    
    - Se añadió la URL correspondiente en backend/users/urls.py.
    
    - Permite a los usuarios cerrar sesión de forma segura.

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
 
 # Create your views here.
@@ -15,3 +16,15 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Sesión cerrada correctamente."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"detail": "Error al cerrar sesión. Token inválido."}, status=status.HTTP_400_BAD_REQUEST)
